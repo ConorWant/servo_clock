2a7cee2fea28b18dcfcd7fdbbdea3934b5d5a7ed
ConorWant <c.want4853@student.leedsbeckett.ac.uk>
2026-04-01 15:46:09 +0100

    Can create numbers on 1 group of 7 servos

--- main.py
+++ main.py
@@ -8,15 +8,54 @@ GROUP1 = list(range(0, 7))
 GROUP2 = list(range(7, 14))
 
 
-def servo_on(kit, group):
-    for ch in group:
+def servo_on(kit, channels):
+    for ch in channels:
         kit.servo[ch].angle = 180
 
-def servo_off(kit, group):
-    for ch in group:
-        kit.servo[ch].angle = 120  
-
+def servo_off(kit, channels):
+    for ch in channels:
+        kit.servo[ch].angle = 120
+          
+def one(kit):
+    channels = [1, 2]
+    servo_on(kit, channels)
         
+def two(kit):
+    channels = [0, 1, 3, 4, 6]
+    servo_on(kit, channels)
+    
+def three(kit):
+    channels = [0, 1, 2, 3, 6]
+    servo_on(kit, channels) 
+       
+def four(kit):
+    channels = [1, 2, 5, 6]
+    servo_on(kit, channels)
+        
+def five(kit):
+    channels = [0, 2, 3, 5, 6]
+    servo_on(kit, channels)
+       
+def six(kit):
+    channels = [0, 2, 3, 4, 5, 6]
+    servo_on(kit, channels)
+        
+def seven(kit):
+    channels = [0, 1, 2]
+    servo_on(kit, channels)
+        
+def eight(kit):
+    channels = [0, 1, 2, 3, 4, 5, 6]
+    servo_on(kit, channels)
+    
+def nine(kit):
+    channels = [0, 1, 2, 3, 5, 6]
+    servo_on(kit, channels)
+       
+def zero(kit):
+    channels = [0, 1, 2, 3, 4, 5]
+    servo_on(kit, channels)
+            
 def main():
     kit1 = ServoKit(channels=16, address=0x40)
     kit2 = ServoKit(channels=16, address=0x41)
@@ -27,20 +66,34 @@ def main():
         kit2.servo[ch].set_pulse_width_range(PULSE_MIN, PULSE_MAX)
         
     servo_off(kit2, GROUP1)
-    servo_state = 0
     
+    
+    NUMBERS = {
+        "0": zero,
+        "1": one,
+        "2": two,
+        "3": three,
+        "4": four,
+        "5": five,
+        "6": six,
+        "7": seven,
+        "8": eight,
+        "9": nine,
+    }
+
     while True:
-        user_input = input("Type to move the servo: ")
-        
+        user_input = input("Type a number: ")
+
         if user_input.lower() == "q":
             break
-        elif servo_state == 0: 
-            servo_on(kit2, GROUP1)
-            servo_state = 1
+        elif user_input in NUMBERS:
+            servo_off(kit2, GROUP1)
+            NUMBERS[user_input](kit2)
+
         else:
             servo_off(kit2, GROUP1)
-            servo_state = 0
+                
 
-        
+            
 if __name__ == "__main__":
     main()
