f20a59a11a681ed141965aa59f294dd3f38ea2f9
ConorWant <c.want4853@student.leedsbeckett.ac.uk>
2026-04-01 15:16:02 +0100

    Control group of 7 servos into on and off positions

--- main.py
+++ main.py
@@ -4,26 +4,18 @@ BOARD1_CHANNELS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 BOARD2_CHANNELS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 PULSE_MIN = 500
 PULSE_MAX = 2500
+GROUP1 = list(range(0, 7))
+GROUP2 = list(range(7, 14))
 
 
-def move_servos(kit, channels, angle):
-    for ch in channels:
-        kit.servo[ch].angle = angle
-        
-def get_angle():
-    user_input = input("Enter an angle between 0 and 180 (q to quit): ")
-    
-    if user_input.lower() == "q":
-        return None
-    try:
-        angle = float(user_input)
-        if not 0 <= angle <= 180:
-            print("Angle must be between 0 and 180")
-            return get_angle()
-        return angle
-    except ValueError:
-        print("Invalid input, enter an angle between 0 and 180 (q to quit)")
-        return get_angle()
+def servo_on(kit, group):
+    for ch in group:
+        kit.servo[ch].angle = 180
+
+def servo_off(kit, group):
+    for ch in group:
+        kit.servo[ch].angle = 120  
+
         
 def main():
     kit1 = ServoKit(channels=16, address=0x40)
@@ -33,16 +25,22 @@ def main():
         kit1.servo[ch].set_pulse_width_range(PULSE_MIN, PULSE_MAX)
     for ch in BOARD2_CHANNELS:
         kit2.servo[ch].set_pulse_width_range(PULSE_MIN, PULSE_MAX)
+        
+    servo_off(kit2, GROUP1)
+    servo_state = 0
     
     while True:
-        angle = get_angle()
+        user_input = input("Type to move the servo: ")
         
-        if angle == None:
-            print("Thank you, Bye!")
+        if user_input.lower() == "q":
             break
-        
-        move_servos(kit1, BOARD1_CHANNELS, angle)
-        move_servos(kit2, BOARD2_CHANNELS, angle)
+        elif servo_state == 0: 
+            servo_on(kit2, GROUP1)
+            servo_state = 1
+        else:
+            servo_off(kit2, GROUP1)
+            servo_state = 0
+
         
 if __name__ == "__main__":
     main()
